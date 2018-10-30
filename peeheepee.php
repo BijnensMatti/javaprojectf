ce7ceb73273e83e181cd53a0e95b28d6af759247
Matti Bijnens <matti.bijnens@student.howest.be>
2018-10-30 16:26:08 +0100

    Decency is key

--- peeheepee.php
+++ peeheepee.php
@@ -8,3 +8,32 @@
 
 
 echo "hi this is a test app";
+
+
+echo "Hello World";
+
+echo "This spans
+multiple lines. The newlines will be
+output as well";
+
+echo "This spans\nmultiple lines. The newlines will be\noutput as well.";
+
+echo "Escaping characters is done \"Like this\".";
+
+// You can use variables inside of an echo statement
+$foo = "foobar";
+$bar = "barbaz";
+
+echo "foo is $foo"; // foo is foobar
+
+// You can also use arrays
+$baz = array("value" => "foo");
+
+echo "this is {$baz['value']} !"; // this is foo !
+
+// Using single quotes will print the variable name, not the value
+echo 'foo is $foo'; // foo is $foo
+
+// If you are not using any other characters, you can just echo variables
+echo $foo;          // foobar
+echo $foo,$bar;     // foobarbarbaz
